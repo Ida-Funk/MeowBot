abe32febd85f1f3233e343197395370c61728654
Ida Funk <ida.funk@se.abb.com>
2025-09-16 16:30:32 +0200

    added move

---
+++ move.h
@@ -0,0 +1,14 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+#include <Arduino.h>
+
+
+// extern const int motorLeft;
+// extern const int motorRight;
+
+void moveSetup();
+void moveForward();
+void moveStop();
+
+#endif // MOVE_H
